4f914125f673d6882bd130c207f1a81239448689
Jonny Lin <jonny@snippyly.com>
2023-02-20 08:02:25 -0800

    Fixed css for overflow and draggable

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,28 +1,31 @@
 import styles from './Content.module.css';
 
 import CardImage from '../../icons/card.svg';
+import Draggable from "react-draggable";
 
 const Content = () => {
 
   return (
-    <div className={styles['content']}>
-      <div className={styles['navbar']}>
-        <div className={styles['logo']}></div>
-        <a contentEditable={true}>Banking</a>
-        <a contentEditable={true}>Cards</a>
-        <a contentEditable={true}>Investments</a>
-      </div>
-      <div className={styles['splash']}>
-        <div className={styles['left-side']}>
-          <h1 contentEditable={true}>One card for everything</h1>
-          <p contentEditable={true}>3% cashback on all purchases</p>
-          <button>Get started</button>
+    <Draggable>
+      <div className={styles['content']}>
+        <div className={styles['navbar']}>
+          <div className={styles['logo']}></div>
+          <a contentEditable={true}>Banking</a>
+          <a contentEditable={true}>Cards</a>
+          <a contentEditable={true}>Investments</a>
         </div>
-        <div className={styles['right-side']}>
-          <img src={CardImage} />
+        <div className={styles['splash']}>
+          <div className={styles['left-side']}>
+            <h1 contentEditable={true}>One card for everything</h1>
+            <p contentEditable={true}>3% cashback on all purchases</p>
+            <button>Get started</button>
+          </div>
+          <div className={styles['right-side']}>
+            <img src={CardImage} />
+          </div>
         </div>
       </div>
-    </div>
+    </Draggable>
   );
 }
 
